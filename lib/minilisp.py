594559243201e31473a074da35c6f82657eada08
pl <0x00.pl@gmail.com>
2014-08-08 01:46:20 +0800

    add operator.cond; \nadd test fib 200.

--- lib/minilisp.py
+++ lib/minilisp.py
@@ -19,9 +19,16 @@ def apply(exp, env):
         return exp
     if is_lambda(exp):
         env= { k:v for k,v in env.items() if k not in exp[1] }
-    return tuple( apply(i,env) for i in exp )
+    return len(env)>0 and tuple( apply(i,env) for i in exp ) or exp
 
 def eval(l):
+    if isinstance(l,Iterable):
+        if l[0]=='cond':
+            for i in range(2,len(l),2):
+                if eval(l[i-1]):
+                    return eval(l[i])
+            return len(l)%2==0 and eval(l[-1])			
+           
     if type(l)==str or\
        isinstance(l,Callable) or\
        is_lambda(l) or\
@@ -38,15 +45,21 @@ def eval_inner(f,*args):
         return f(*args)
     if is_lambda(f):
         _,names,exp= f
-        tail= apply(exp,dict(zip(names,args)))
+        tail= apply(exp,dict(zip(names, (f,)+args)))
         return eval(tail)
     handle_err(str(f)+'is not a function.')
 
 if __name__=='__main__':
     import operator as op
-    exp= ((("lambda", ('a',), 
-                      ("lambda", ("b",),(op.add, 'b', 'a')))
+    addn= ((("lambda", ('self','a'), 
+                      ("lambda", ('self',"b",),(op.add, 'b', 'a')))
            ,(op.add,20,20))
           ,(op.add,1,1))
+
+    fib=('lambda',('fib','a1','a2','n'),
+         ('cond',(op.ge,2,'n'),'a2',
+          True,('fib','a2',(op.add,'a1','a2'),(op.sub,'n',1))))
+    exp= (fib,1,1,200)
     print(exp)
     print(eval(exp))
+
